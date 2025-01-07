e64f4939b4696c21780aed2b6db8bd840a3138fb
Micah <killickster@gmail.com>
2025-01-07 16:39:47 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'histogram-component';
+  data: any; // Variable to hold the JSON data
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  // Method to load JSON data
+  loadData(): void {
+    this.http.get('assets/d1.json').subscribe((response) => {
+      console.log(response); // For debugging
+      this.data = response;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { TrackingStatisticsHistogramComponent } from './tracking-statistics-histogram/tracking-statistics-histogram.component';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrackingStatisticsHistogramComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
